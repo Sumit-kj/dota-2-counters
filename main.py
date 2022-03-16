87c42330210c43c8462502140c2ae42e71bb4b6d
Sumit-kj <sumitkj7.5@gmail.com>
2022-03-16 23:55:52 +0530

    Adapted to FastAPI

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 
-import matplotlib.pyplot as plt
+import uvicorn
+from fastapi import FastAPI
 import alias as al
-import warnings
-import matplotlib.cbook
-warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
+
+app = FastAPI()
 
 
 def create_counter_dict():
@@ -15,40 +15,6 @@ def create_counter_dict():
     with open('counters.json', 'r') as fp:
         h_c_dict = json.load(fp)
         return h_c_dict
-
-
-def get_opponents_pick(h_c_dict):
-    """
-    This function takes opponent's pick as input
-    :return: this function returns the input from user
-    """
-
-    opp_picks = []
-    i = 0
-    while i < 5:
-        pick_message = 'Enter pick ' + str(i + 1) + ": "
-        pick = str(input(pick_message)).upper()
-        try:
-            if al.alias[pick] in opp_picks:
-                raise Exception('Same hero present!')
-            opp_picks.append(al.alias[pick])
-
-            counter_tally_dict = get_counter_tally(opp_picks, h_c_dict)
-            counter_tally_dict = get_counter_tally_sorted(counter_tally_dict)
-            # counter_tally_dict = get_counter_to_counter_tally(opp_picks, counter_tally_dict, h_c_dict)
-            plot = plot_counter_bar_plot(counter_tally_dict)
-            # plot.show()
-            plot.draw()
-            plot.pause(0.001)
-            input('Press Enter to continue')
-            if i != 4:
-                plot.close('all')
-            i += 1
-        except KeyError:
-            print('Enter the hero again!')
-        except Exception as e:
-            print(e)
-    return opp_picks
 
 
 def get_counter_tally(opp_picks, h_c_dict):
@@ -84,36 +50,14 @@ def get_counter_tally_sorted(c_t_dict):
     return {k: v for k, v in sorted(c_t_dict.items(), key=lambda item: item[1], reverse=True)}
 
 
-def plot_counter_bar_plot(data):
-    """
-    This function plots bar plot for the counters
-    :return: None
-    """
-    counters = list(data.keys())[:30]
-    values = list(data.values())[:30]
-
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax.barh(counters, values)
-    for s in ['top', 'bottom', 'left', 'right']:
-        ax.spines[s].set_visible(False)
-    ax.xaxis.set_ticks_position('none')
-    ax.yaxis.set_ticks_position('none')
-    ax.xaxis.set_tick_params(pad=5)
-    ax.yaxis.set_tick_params(pad=10)
-    ax.grid(b=True, color='grey',
-            linestyle='-.', linewidth=0.5,
-            alpha=0.2)
-    ax.invert_yaxis()
-    for i in ax.patches:
-        plt.text(i.get_width() + 0.2, i.get_y() + 0.5,
-                 str(round((i.get_width()), 2)),
-                 fontsize=10, fontweight='bold',
-                 color='grey')
-    ax.set_title('Counters',
-                 loc='left', )
-    return plt
-
-
-if __name__ == '__main__':
+@app.get('/get_counters')
+async def get_counter_picks(picks: str = ''):
+    opponent_picks = [al.alias[x.strip().upper()] for x in picks.split(',')]
     hero_counter_dict = create_counter_dict()
-    opponents_picks = get_opponents_pick(hero_counter_dict)
+    result = get_counter_tally(opponent_picks, hero_counter_dict)
+    result = get_counter_tally_sorted(result)
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000)
